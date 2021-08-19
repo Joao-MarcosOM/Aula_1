c86dc46b6510b0d40a1290e215bf0e6da1893284
Joao-MarcosOM <joaomarcosbigsan@sempreceub.com>
2021-08-19 11:19:11 -0300

    Commit do Servlet com atualização Data e Hora

---
+++ src/main/java/Aula3/ServletDataHora.java
@@ -0,0 +1,50 @@
+package Aula3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDataHora
+ */
+@WebServlet("/ServletDataHora")
+public class ServletDataHora extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy - hh:mm:ss");
+	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDataHora() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	// TODO Auto-generated method stub
+    	super.init();
+    	log("ServletDataHora iniciado as" + format.format(new Date()));
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter saida = response.getWriter();
+		saida.write("<HTML><BODY>Olá, a data e hora atual é: ");
+		saida.write(format.format(new Date()));
+		saida.write("</BODY></HTML>");
+		saida.close();
+	}
+
+}
---
+++ src/main/java/Aula3/ServletGetPost.java
@@ -0,0 +1,50 @@
+package Aula3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletGetPost
+ */
+@WebServlet("/ServletGetPost")
+public class ServletGetPost extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletGetPost() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter saida = response.getWriter();
+		saida.write("<HTML><BODY>Olá");
+		saida.write("Resposta do método GET");
+		saida.write("</BODY></HTML>");
+		saida.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter saida = response.getWriter();
+		saida.write("<HTML><BODY>Olá");
+		saida.write("Resposta do método POST");
+		saida.write("</BODY></HTML>");
+		saida.close();
+	}
+
+}
